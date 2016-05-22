50e9e400f2f40745a971b424337edc503a18bb6a
Delmond <muha.delalic@hotmail.com>
2016-05-22 12:56:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connection = mysql.createConnection({
 });
 
 var app = express();
-
+var router = express.Router();
 connection.connect(function(err){
 
 if(!err){
@@ -22,40 +22,82 @@ if(!err){
 });
 
 
-app.get("/", function(req,res){
+router.get("/", function(req,res){
 
 connection.query('SELECT * FROM Users;',function(err,rows,fields){
 	if(!err){
 		console.log(rows);
+		res.setHeader('Content-Type', 'application/json');
 		res.status(200).send(rows);}
 	else{
 		console.log("Greska");
-		res.status(400).send(rows);
+		res.status(400).send();
 }
 
 });
 });
-app.get("/register",function(req, res){
+router.get("/register",function(req, res){
 
-var username = req.query.username;
-var password = req.query.password;
-var email = req.query.email;
-if(username == null || password == null || email== null){
-	console.log("/register with incorrect parametars called!");
-	console.log(username, password,email);
-	res.status(400).send();
-} else {
-	console.log("/register good");
-	conneciton.query("INSERT INTO Users (username,password,email) VALUES (username,password,email);",
-function(error, rows, field){
-	if(!error){
-		console.log("Sve uspjelo");
-		console.log(rows);
-		res.status(200).send();
-	}else{
-		console.log("Greska pri ubaciavanju!");
+	var username = req.query.username;
+	var password = req.query.password;
+	var email = req.query.email;
+	if(username == null || password == null || email== null){
+		console.log("/register with incorrect parametars called!");
+		console.log(username, password,email);
+		res.status(400).send();
+	} else {
+		console.log("/register good");
+		console.log(username, password,email);
+		connection.query("INSERT INTO Users (username,password,email) VALUES (username,password,email);",
+	function(error, rows, field){
+		if(!error){
+			console.log("Sve uspjelo");
+			console.log(rows);
+			res.status(200).send();
+		}else{
+			console.log("Greska pri ubaciavanju!");
+			res.status(400).send();
+		}
+	});
 	}
 });
-}
+router.get("/register",function(req, res){
+
+	var username = req.query.username;
+	var password = req.query.password;
+	var email = req.query.email;
+	if(username == null || password == null || email== null){
+		console.log("/register with incorrect parametars called!");
+		console.log(username, password,email);
+		res.status(400).send();
+	} else {
+		console.log("/register good");
+		console.log(username, password,email);
+		connection.query("INSERT INTO Users (username,password,email) VALUES (username,password,email);",
+	function(error, rows, field){
+		if(!error){
+			console.log("Sve uspjelo");
+			console.log(rows);
+			res.status(200).send();
+		}else{
+			console.log("Greska pri ubaciavanju!");
+		}
+	});
+	}
 });
+router.get("/login", function(req, res){
+	var username = req.query.username;
+	var password = req.query.email;
+	connection.query("SELECT EXISTS(SELECT * FROM Users WHERE username="+username +	" AND email=" + email+");",
+	 	function(error, rows, field){
+			if(!error){
+				console.log("Uspjesan Login");
+				console.log(rows);
+				res.status(200).send("Login Succesful!");
+			}else{
+				res.status(400).send("Login Unsuccesful!");
+			}
+
+	});
+})
 app.listen(3000);
